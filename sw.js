dbb45ae64aefe27c4a6ba3851562a95de27732be
youssef <yousseflahsaniiiiii@gmail.com>
2026-03-18 00:11:52 +0000

    Update service worker zoneId to 10744730

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10744728
+    "zoneId": 10744730
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
